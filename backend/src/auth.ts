dbc1e8c362068accf44a3bc458b47acbc6ed01d1
paulocerez <paulo@telli.com>
2025-07-28 00:12:12 +0200

    betterauth config

--- backend/src/auth.ts
+++ backend/src/auth.ts
@@ -8,8 +8,10 @@ export const auth = betterAuth({
   }),
   socialProviders: {
     google: {
-      clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      accessType: "offline", 
+      prompt: "select_account+consent", 
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     },
   },
 });
